b61d239dd9a0fc62c59d79e9c3deda08b2282e8d
unknown <@oleg-pc.ntcnvg.ru>
2014-02-07 15:27:46 +0400

    add Gob's program

---
+++ Gobs-Program.ps1
@@ -0,0 +1,32 @@
+$ui = $host.ui.rawUI
+$prevBackColor = $ui.BackgroundColor
+$prevForeColor = $ui.ForegroundColor
+$ui.BackgroundColor = "black"
+$ui.ForegroundColor = "green"
+    
+try{
+    Clear-Host
+    Write-Host ("`n" * $ui.windowSize.height )
+    Write-Host "Gob's Program: Y/N?" 
+    Write-Host '? ' -non 
+
+    $press = [console]::readKey() 
+
+    if( -not ($press.keyChar -match 'y') ) {
+        Exit
+    }
+
+    Write-Host "`n"
+    $penusCount = 0
+    for( ;;){
+        Write-Host "Penus " -noNewLine 
+        Start-Sleep -m  ( [int] ([math]::log($penusCount + 1) / 15)  ) 
+        $penusCount++
+        if($penusCount % 6 -eq 0 ) { Write-Host '' }
+    }
+}finally{
+   Write-Host "`n"
+   $ui.BackgroundColor = $prevBackColor
+   $ui.ForegroundColor = $prevForeColor
+}
+ 
